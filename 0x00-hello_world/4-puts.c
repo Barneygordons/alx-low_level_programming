3f0e1276f85f1f67f488ce84c9060f1a27b2e5c3
Barneygordons <barnygordons@gmail.com>
2022-09-11 14:05:35 -0700

    Made some corrections on the file to observe Betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,7 @@
 
 
 /**
- *
- *  main - prints the phrase "Programming is like building a multilingual puzzle
+ *  main - prints "Programming is like building a multilingual puzzle
  *
  *  Return: 0 if exited properly, non-zero otherwise
  *
@@ -14,8 +13,8 @@ int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+		return (0);
 
 }
