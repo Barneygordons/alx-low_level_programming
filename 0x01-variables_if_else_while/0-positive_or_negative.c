597ce1d6eb6960cf462df9b5a681839ff978c2aa
Barneygordons <barnygordons@gmail.com>
2022-09-12 16:28:04 -0700

    Made some adjustments to observe betty coding style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,6 @@
 
 
 /**
-  *
   * main - assigns a random number to int n everytime
   *
   * it executes, and prints it
@@ -27,15 +26,15 @@ int main(void)
 
 	if (n > 0)
 
-	printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 
 	else if (n == 0)
 
-	printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 
 	else if (n < 0)
 
-	printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 
 		return (0);
 
